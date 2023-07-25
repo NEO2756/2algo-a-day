81d9e9f887f85ff50496d876cca179d820e622fa
Sandeep <sandeep.jack2756@gmail.com>
2023-07-25 11:48:18 +0100

    Update 1.go

--- src/1.go
+++ src/1.go
@@ -13,3 +13,37 @@ func TwoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+
+// LengthOfLongestSubstring 2 lengthOfLongestSubstring
+func LengthOfLongestSubstring(s string) int {
+    
+    if len(s) == 0 {
+        return 0
+    }
+    freq := make([]int, 255)
+    ans := 1
+    freq[s[0]] = 1
+    max := 1
+    for start , end := 0, 1; end < len(s); end++ {
+        if freq[s[end]] > 0 && freq[s[end]] >= start {
+            start = freq[s[end]]
+            freq[s[end]] = end + 1
+            max = end - start + 1
+            ans = maxi(ans, max)
+            continue
+        }
+        freq[s[end]] = end+1
+        max++
+        ans = maxi(ans, max)
+        
+    }
+    
+    return ans
+}
+
+func maxi(x,y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
